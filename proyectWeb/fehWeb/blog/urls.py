550452bbe14f181301fbfab83eed003eb0578c95
Abidael <abidael.666@gmail.com>
2021-01-26 07:09:22 -0300

    Commit ajilao cabros

--- proyectWeb/fehWeb/blog/urls.py
+++ proyectWeb/fehWeb/blog/urls.py
@@ -3,9 +3,12 @@ from . import views
 
 urlpatterns = [
     path('', views.portada),
-    path('post_list/', views.post_list),
+    path('post_list/', views.post_list, name='post_list'),
     path('portada/', views.post_list),
     path('heroes/', views.heroes),
     path('login/', views.login),
     path('questions/', views.formulario),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit', views.post_edit, name='post_edit'),
 ]
--- proyectWeb/fehWeb/blog/views.py
+++ proyectWeb/fehWeb/blog/views.py
@@ -17,12 +17,31 @@ def heroes(request):
 def login(request):
     return render(request, 'plantillas/login.html', {})
 
+
+def formulario(request):
+    return render(request, 'plantillas/questions.html', {})
+
+
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'plantillas/post_list.html', {'posts': posts})
+
+
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'plantillas/post_detail.html', {'post': post})
 
 def post_new(request):
-    form = PostForm()
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
     return render(request, 'plantillas/post_edit.html', {'form': form})
 
 def post_edit(request, pk):
@@ -37,16 +56,4 @@ def post_edit(request, pk):
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
-    return render(request, 'plantilla/post_edit.html', {'form': form})
-
-def formulario(request):
-    return render(request, 'plantillas/questions.html', {})
-
-
-def publicaciones(request):
-    return render(request, 'plantillas/post_list.html', {})
-
-
-def post_list(request):
-    posts = Post.objects.all()
-    return render(request, 'plantillas/post_list.html', {'posts': posts})
+    return render(request, 'plantillas/post_edit.html', {'form': form})
