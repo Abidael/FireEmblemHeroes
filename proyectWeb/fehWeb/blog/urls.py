68c21efd9166ed7b999a3aba1e09dd02f977e6f2
Abidael <abidael.666@gmail.com>
2021-01-26 06:22:24 -0300

    joven

--- proyectWeb/fehWeb/blog/urls.py
+++ proyectWeb/fehWeb/blog/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [   
     path('', views.portada),
     path('post_list/', views.post_list),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
--- proyectWeb/fehWeb/blog/views.py
+++ proyectWeb/fehWeb/blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
 
 
 # Create your views here.
@@ -12,5 +13,26 @@ def post_list(request):
     posts = Post.objects.all()
     return render(request, 'plantillas/post_list.html', {'posts': posts})
 
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'plantillas/post_detail.html', {'post': post})
+
+def post_new(request):
+    form = PostForm()
+    return render(request, 'plantillas/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'plantilla/post_edit.html', {'form': form})
 
 
